4c441bdf56c8faa5c945b1161f3ececa10efbec7
Alex <amarvick94@gmail.com>
2019-04-01 10:08:57 -0700

    Deleting src file, starting from scratch

--- src/actionTypes/authActionTypes.js
+++
@@ -1,8 +0,0 @@
-/* File Name: authActionTypes.js                                            *
- * Description: Action Types for auth                                       */
-
-const AuthActionTypes = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER'
-}
-
-export default AuthActionTypes
